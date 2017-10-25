43506c99e3ade76a74f7286280a617cd05a7a861
twairball <twairball@yahoo.com>
2017-10-25 23:16:53 +0800

    update

--- main/train_base.sh
+++ main/train_base.sh
@@ -5,7 +5,6 @@ mkdir -p ./data/wmt_base
 mkdir -p ./train/wmt_base
 t2t-trainer \
   --t2t_usr_dir=./data_generators \
-  --generate_data \
   --data_dir=./data/wmt_base \
   --problems=translate_enzh_wmt_base \
   --model=transformer \
